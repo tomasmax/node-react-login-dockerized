ada2edbda3c46862194992ab9e2317a9e53636f2
Tomas Madariaga <critimass@gmail.com>
2020-08-01 03:52:36 +0200

    feat: login app front end

---
+++ client/src/test-utils.js
@@ -0,0 +1,22 @@
+// test-utils.js
+import React from 'react'
+import { render as rtlRender } from '@testing-library/react'
+import { createStore } from 'redux'
+import { Provider } from 'react-redux'
+import { initialState } from './redux/reducers'
+
+function render(ui, { reducer, ...renderOptions } = {}) {
+  const dummy = () => {}
+  const store = createStore(reducer || dummy, initialState)
+  console.log('#CC store ', store.getState())
+  function Wrapper({ children }) {
+    return <Provider store={store}>{children}</Provider>
+  }
+  return rtlRender(ui, { wrapper: Wrapper, ...renderOptions })
+}
+
+// re-export everything
+export * from '@testing-library/react'
+
+// override render method
+export { render }
